ad377f2c4997f29cb387b698914dd16b44ce8bd9
blob8129 <blob8129@gmail.com>
2024-02-02 17:22:36 +0100

    initial commit

---
+++ MetricMusic/MetricMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetricMusicApp.swift
+//  MetricMusic
+//
+//  Created by Andrey Volobuev on 02/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct MetricMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
